4cc6ec104e8c8747f410a37a9e1db26807e3d75b
deniSlow <denis.kuznetsov.new@gmail.com>
2026-04-29 12:07:23 +0400

    Autoscaling fix remove attachment

--- main.tf
+++ main.tf
@@ -77,12 +77,6 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
-}
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.2.0"
@@ -99,7 +93,7 @@ module "blog_autoscaling" {
   instance_type   = var.instance_type
   image_id        = data.aws_ami.app_ami.id
 
-  traffic_source_attachments {
+  traffic_source_attachments = {
     blog-alb {
       traffic_source_identifier = aws_lb_target_group.blog.arn
     }
